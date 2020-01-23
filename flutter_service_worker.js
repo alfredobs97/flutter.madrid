499643cb7a2718cb8801454d4381acad4f5b821a
jamesblasco <git@jaimeblasco.com>
2020-01-23 15:35:16 +0000

    :rocket: Roll master - Deploy website

---
+++ flutter_service_worker.js
@@ -0,0 +1,46 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/assets/assets/logo.png": "ec785d54083459b46a0451d3e44f7935",
+"/assets/assets/background.png": "f495043f7ddd143e12abfa0874a41f29",
+"/assets/assets/meetup_10_02_2020.png": "02397b0f751438d1383a5d7eaeeb563c",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/AssetManifest.json": "4255bfa820d6a3741bdb0835b3d488e0",
+"/assets/LICENSE": "fbe77d112f4eceaf2a0b1e28a805300d",
+"/assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/manifest.json": "cc712f6a0edaeaf145360671780a09d0",
+"/index.html": "18538c895e1f888c63e86008a80b1d5e",
+"/main.dart.js": "6152b030758e4b56392fd39f238fd4fa"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
